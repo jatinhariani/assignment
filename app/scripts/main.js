9ed37e4c700e60baab785964e25fb164d6a0671d
Jatin Hariani <jtnh321@gmail.com>
2017-12-02 18:14:24 +0530

    slider and lead

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+/* global jQuery */
+jQuery(document).ready(function (e) {
+  jQuery('#background-slider').slick({
+    arrows: false,
+    draggable: false,
+    swipeToSlide: false
+  })
+  jQuery('#content-slider').slick({
+    asNavFor: '#background-slider',
+    fade: true,
+    prevArrow: '<button type="button" class="btn btn-primary slick-prev"><i class="fa fa-angle-left"></i></button>',
+    nextArrow: '<button type="button" class="btn btn-primary slick-next"><i class="fa fa-angle-right"></i></button>'
+  })
+})
